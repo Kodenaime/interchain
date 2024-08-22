aeefd4ab12e0c1e28575e31468f35d767f43d9be
iamkodenaime <creativedannyk@gmail.com>
2024-08-22 04:54:53 +0100

    uploaded assets

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './navbar.css'
+import logo from'../../Assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+      <ul className="nav-items">
+        <li><a href="">Features</a></li>
+        <li><a href="">How it Works</a></li>
+        <li><a href="">Supported Chains</a></li>
+        <li><a href="">Join Community</a></li>
+      </ul>
+      <div className="btn">Launch Dapp</div>
+    </div>
   )
 }
 
