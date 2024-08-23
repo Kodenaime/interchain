b9815e06a0c61c75c1f284ebf9eb38ad8216c204
iamkodenaime <creativedannyk@gmail.com>
2024-08-23 19:06:31 +0100

    features section started

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './features.css'
+import blockchain from '../../Assets/blockchain.png'
+import cyber from '../../Assets/cyber-security.png'
+import deadline from '../../Assets/deadline.png'
+import responsive from '../../Assets/responsive.png'
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className="wrapper">
+        <section className="features container">
+      <div className="left">
+          <h5>Features</h5>
+          <h2>Interchain <br /> Key <br /> Features</h2>
+      </div>
+      
+      <div className="right">
+          <div className="fcards">
+            <i><img src={blockchain} alt="" /></i>
+            <h4>Supports multiple blockchains</h4>
+            <p>InterChain bridges blockchain ecosystems like Ethereum, Solana, and Polygon, enabling seamless 
+              token transfers across different networks.</p>
+          </div>
+          <div className="fcards">
+            <i><img src={cyber} alt="" /></i>
+            <h4>Security and privacy</h4>
+            <p>Advanced security measures ensure your assets are protected with multi-signature
+               wallets and encryption protocols.</p>
+          </div>
+          <div className="fcards">
+            <i><img src={deadline} alt="" /></i>
+            <h4>Speed and efficiency</h4>
+            <p>Swift transactions and competitive fees make InterChain the
+               ideal choice for efficient cross-chain transfers.</p>
+          </div>
+          <div className="fcards">
+            <i><img src={responsive} alt="" /></i>
+            <h4>User-friendly interfacee</h4>
+            <p>Swift transactions and competitive fees make InterChain the ideal
+               choice for efficient cross-chain transfers.</p>
+          </div>
+      </div>
+      
+    </section>
+    <div className="border"></div>
+    </div>
+    
   )
 }
 
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './hero.css'
+import chain from '../../Assets/chain.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="wrapper">
+        <section className="hero container">
+      <div className="left">
+        <h1>
+          Seamless <br /> Cross-Chain <br /> Transfers
+        </h1>
+        <h4>
+          InterChain enables you transfer assets <br /> between blockchains quickly and securely
+        </h4>
+        <div className="btns">
+          <div className="btn">
+            Get started
+          </div>
+          <div className="btn-primary">
+            learn more
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img src={chain} alt="" />
+      </div>
+    </section>
+
+    <div className="border"></div>
+
+
+    </div>
+    
   )
 }
 
