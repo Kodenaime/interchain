3d07566e6507caeca8512728476f5f059e10cd81
iamkodenaime <creativedannyk@gmail.com>
2024-08-26 21:50:37 +0100

    finished with features

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './about.css'
+import note from '../../Assets/notebook.png'
+import small1 from '../../Assets/small1.png'
+import small2 from '../../Assets/small2.png'
+import small3 from '../../Assets/small3.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="wrapper">
+      <section className="about container">
+
+        <div className="about-left">
+          <img src={note} alt="" />
+        </div>
+
+        <div className="about-right">
+          <h5>How it Works</h5>
+          <h2>How to use InterChain</h2>
+          <article className="article">
+            <i className="about-icon"><img src={small1} alt="" /></i>
+              
+              <p>Connect Wallet - Begin by connecting your preferred Web3 wallet,
+                 such as MetaMask or Trust Wallet, to our platform.</p>
+            
+          </article>
+          <article className="article">
+            <i className="about-icon">
+              <img src={small2} alt="" /></i>
+              <p>Connect Wallet - Begin by connecting your preferred Web3 wallet,
+                 such as MetaMask or Trust Wallet, to our platform.</p>
+            
+          </article>
+          <article className="article">
+            <i className="about-icon">
+              <img src={small3} alt="" />  </i>
+              <p>Connect Wallet - Begin by connecting your preferred Web3 wallet,
+                 such as MetaMask or Trust Wallet, to our platform.</p>
+          
+          </article>
+        </div>
+
+      </section>
+
+      <div className="border"></div>
+
+    </div>
   )
 }
 
--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './support.css'
+import icon1 from '../../Assets/icon1.png'
 
 const Support = () => {
   return (
-    <div>Support</div>
+    <section className="support">
+      <h5>supported chains</h5>
+      <h2>List of Supported Chains</h2>
+      <div className="sup-icons">
+        <img src={icon1} alt="" />
+      </div>
+    </section>
   )
 }
 
